db2768c3c261527d99aa3ee25977c1f5b26f1c6f
jiwoo jeon <jjw2995@gmail.com>
2024-02-15 16:58:01 -0800

    f

--- src/app/stores/page.tsx
+++ src/app/stores/page.tsx
@@ -5,6 +5,15 @@ import { LogOut } from "lucide-react";
 import CreateStore from "./StoreCreate";
 import { api } from "~/trpc/react";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+
 export default function Stores() {
   const stores = api.store.get.useQuery();
 
@@ -13,6 +22,7 @@ export default function Stores() {
       <div className="flex w-screen max-w-[40rem] flex-col">
         <div className="mx-6 my-4 flex items-end justify-between">
           <h1 className="text-4xl font-bold tracking-wide">Dojo</h1>
+
           <Link href={"/api/auth/signout"}>
             <LogOut className="h-7 w-7" />
           </Link>
@@ -22,16 +32,16 @@ export default function Stores() {
             alt=""
           /> */}
         </div>
-        <div className="m-4 flex flex-col">
+        <div className="m-4 flex flex-col space-y-2">
           {stores.data ? (
             stores.data.map(({ store }) => {
               return (
-                <Link
-                  href={`/stores/${store.id}/home`}
-                  key={store.id}
-                  className="m-1 rounded p-2 text-2xl outline"
-                >
-                  {store.name}
+                <Link href={`/stores/${store.id}/home`} key={store.id}>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle className="text-xl">{store.name}</CardTitle>
+                    </CardHeader>
+                  </Card>
                 </Link>
               );
             })
