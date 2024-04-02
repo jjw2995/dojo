6f86c5a8c5109ca70d25a174d6a062519de7f6cb
Jiwoo Jeon <jjw2995@gmail.com>
2024-04-02 00:06:55 -0700

    feat: pwa for app-like experience

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,30 @@
  * for Docker builds.
  */
 await import("./src/env.mjs");
+import withPWAInit from "@ducanh2912/next-pwa";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  reactStrictMode: true,
 
-export default config;
+  /**
+   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+   * out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+
+const withPWA = withPWAInit({
+  dest: "public",
+  // disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/app",
+  sw: "service-worker.js",
+});
+
+export default withPWA(config);
