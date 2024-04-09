64173d1c9f1ea9297a5c4178115d1b06082ac8e8
Jiwoo Jeon <jjw2995@gmail.com>
2024-04-08 22:19:53 -0700

    feat(options): create component layout done

--- src/app/stores/[storeId]/items/(comps)/options.tsx
+++ src/app/stores/[storeId]/items/(comps)/options.tsx
@@ -1,6 +1,7 @@
 import {
   Dialog,
   DialogContent,
+  DialogFooter,
   DialogTitle,
   DialogTrigger,
 } from "~/components/ui/dialog";
@@ -12,7 +13,15 @@ import { useState } from "react";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { Button } from "~/components/ui/button";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 
 export default function Options() {
   return (
@@ -22,7 +31,7 @@ export default function Options() {
           <Plus className="h-4 w-4" />
         </Button>
       </DialogTrigger>
-      <DialogContent className="h-[60%]">
+      <DialogContent className="">
         <Tabs defaultValue="create">
           <DialogTitle className="mb-2">Options</DialogTitle>
           <TabsList className="w-full">
@@ -41,84 +50,117 @@ export default function Options() {
 
 type OptionInput = {
   optionName: string;
-  numChoices: number;
   minSelect: number;
   maxSelect: number;
-
-  options: { name: string; price: number }[];
+  choices: { name: string; price: number }[];
 };
 function OptionCreate() {
   const form = useForm<OptionInput>();
   //   error with useFieldArray, it just says string not assignable to
-  const options = useFieldArray({ name: "options", control: form.control });
+  const choices = useFieldArray({ name: "choices", control: form.control });
 
-  const numChoices = form.watch("numChoices", 0);
-
-  console.log(form.watch());
-  console.log(numChoices);
-
-  function getInteger(strNum: string | undefined | number) {
-    if (!strNum) return 0;
-    return Number(Number(strNum).toFixed(0));
-  }
-
+  const choicesArr = form.watch("choices", []);
+  console.log(choicesArr.length);
   return (
-    <form className="flex flex-col space-y-2">
-      <Input
-        placeholder="option name"
-        {...form.register("optionName", { required: true })}
-      />
-
+    <div className="mt-6 flex flex-col space-y-3">
       <div className="flex items-center justify-between">
-        <Label htmlFor="">number of choices</Label>
+        <Label htmlFor="">option name</Label>
         <Input
-          type="number"
-          className="w-20"
-          {...form.register("numChoices", {
-            // required: true,
-            valueAsNumber: true,
-            value: 0,
-          })}
+          className="w-60 text-end"
+          placeholder="option name"
+          {...form.register("optionName", { required: true })}
         />
       </div>
-
-      <div className="flex items-center justify-between">
-        <Label htmlFor="">toggle at least...</Label>
-        <Input
-          type="number"
-          className="w-20"
-          {...form.register("minSelect", {
-            required: true,
-            valueAsNumber: true,
-            value: 0,
-            disabled: numChoices < 1,
-          })}
-        />
-      </div>
-
-      <div className="flex items-center justify-between">
-        <Label htmlFor="">toggle at most...</Label>
-        <Input
-          type="number"
-          className="w-20"
-          {...form.register("maxSelect", {
-            required: true,
-            valueAsNumber: true,
-            disabled: numChoices < 1,
-          })}
-        />
-      </div>
-      <div className="flex h-[10rem] overflow-y-auto">
-        <div className="flex-1">
-          {/* form.watch("numChoices", 0) CLAIMS its returning a number
-          IT IS NOT */}
-          {Array(getInteger(numChoices))
-            .fill({ name: "name", price: 0 })
-            .map((r, i) => {
-              return <div>???{i}</div>;
+      <div className="flex items-end justify-between">
+        <div className="text-center">
+          <Label htmlFor="">min select</Label>
+          <Input
+            type="number"
+            className="w-20 text-end"
+            {...form.register("minSelect", {
+              required: true,
+              valueAsNumber: true,
+              value: 0,
+              disabled: choicesArr.length < 1,
             })}
+          />
+        </div>
+
+        <div className="text-center">
+          <Label htmlFor="">max select</Label>
+          <Input
+            type="number"
+            className="w-20 text-end"
+            {...form.register("maxSelect", {
+              required: true,
+              valueAsNumber: true,
+              disabled: choicesArr.length < 1,
+            })}
+          />
+        </div>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            choices.append({ name: "", price: 0 });
+          }}
+        >
+          add
+          <br />
+          choice
+        </Button>
+      </div>
+
+      <div className="py-4">
+        <div className="h-[14rem] overflow-y-auto">
+          {choices.fields.map((field, index) => {
+            let onFocus = false;
+            return (
+              <Card key={field.id} className="relative m-2 p-4">
+                <Button
+                  variant="ghost"
+                  className="absolute left-1 top-1 m-0 flex h-6 w-6 rounded-full p-0 focus:bg-red-500 focus:text-accent"
+                  onBlur={() => {
+                    onFocus = false;
+                  }}
+                  onClick={(e) => {
+                    if (onFocus) {
+                      choices.remove(index);
+                    }
+                    onFocus = true;
+                    // choices.remove()
+                  }}
+                >
+                  <X className="h-4 w-4 translate-x-[0.5px]" />
+                </Button>
+                <CardContent className="ml-4 space-y-1 p-0">
+                  <div className="flex items-center justify-between tracking-tight">
+                    <Label className="text-sm leading-tight">option name</Label>
+                    <Input
+                      {...form.register(`choices.${index}.name` as const, {
+                        required: true,
+                      })}
+                      className="w-36"
+                    />
+                  </div>
+                  <div className="flex items-center justify-between tracking-tight">
+                    <Label className="text-sm leading-tight">price</Label>
+                    <Input
+                      type="number"
+                      placeholder="price"
+                      {...form.register(`choices.${index}.price` as const, {
+                        required: true,
+                        valueAsNumber: true,
+                      })}
+                      className="w-36"
+                    />
+                  </div>
+                </CardContent>
+              </Card>
+            );
+          })}
         </div>
       </div>
-    </form>
+      <Button>create option</Button>
+    </div>
   );
 }
